89722ce156ac79a72701cf6ae7359fe34cb12385
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-08-16 22:19:20 +0800

    title logo background color change

--- index.py
+++ index.py
@@ -5,8 +5,15 @@ pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
 
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load("imgs.png")
+pygame.display.set_icon(icon)
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
+    screen.fill((0, 0, 0))
+    pygame.display.update()
