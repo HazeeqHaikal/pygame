a466dea98c558e63c18c0cfadfc31566314e44b8
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-08-16 21:18:00 +0800

    CREATE WINDOW

---
+++ index.py
@@ -0,0 +1,12 @@
+# from turtle import Screen
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
