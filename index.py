8d27bb4191a887f84bdfae3512c36354b15ee7df
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-08-18 19:19:54 +0800

    keybinding

--- index.py
+++ index.py
@@ -13,6 +13,7 @@ playerIMG = pygame.image.load('ufo.png')
 playerX = 370
 playerY = 480
 playerXchange = 0
+playerYchange = 0
 
 def player(x, y):
     screen.blit(playerIMG, (x, y))
@@ -20,10 +21,33 @@ def player(x, y):
 running = True
 while running:
     screen.fill((0, 0, 0))
-    playerY -= 0.2
     for event in pygame.event.get():
+        
         if event.type == pygame.QUIT:
             running = False
-    
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerXchange = -0.1
+            
+            if event.key == pygame.K_RIGHT:
+                playerXchange = 0.1
+                
+            if event.key == pygame.K_UP:
+                playerYchange = -0.1
+                
+            if event.key == pygame.K_DOWN:
+                playerYchange = 0.1
+                
+                
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerXchange = 0
+            
+            if event.key == pygame.K_DOWN or event.key == pygame.K_UP:
+                playerYchange = 0
+            
+    playerX += playerXchange
+    playerY += playerYchange
     player(playerX, playerY)
     pygame.display.update()
