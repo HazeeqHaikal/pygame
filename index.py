ecbbe83465160b89edbdf758c3a551c197bce879
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-08-18 19:02:37 +0800

    moving ufo

--- index.py
+++ index.py
@@ -9,11 +9,21 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load("imgs.png")
 pygame.display.set_icon(icon)
 
+playerIMG = pygame.image.load('ufo.png')
+playerX = 370
+playerY = 480
+playerXchange = 0
+
+def player(x, y):
+    screen.blit(playerIMG, (x, y))
+
 running = True
 while running:
+    screen.fill((0, 0, 0))
+    playerY -= 0.2
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
-    screen.fill((0, 0, 0))
+    player(playerX, playerY)
     pygame.display.update()
