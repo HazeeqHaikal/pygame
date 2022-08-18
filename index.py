297dfa1c31dece4023dbe8e3e057cf6051fc3099
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-08-19 00:35:00 +0800

    border

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 # from turtle import Screen
 import pygame
+import random
 
 pygame.init()
 
@@ -13,10 +14,18 @@ playerIMG = pygame.image.load('ufo.png')
 playerX = 370
 playerY = 480
 playerXchange = 0
-playerYchange = 0
+# playerYchange = 0
+
+enemyIMG = pygame.image.load('asteroid.png')
+enemyX = random.randint(0, 800)
+enemyY = random.randint(50, 150)
+enemyXchange = 0
 
 def player(x, y):
     screen.blit(playerIMG, (x, y))
+
+def enemy(x, y):
+    screen.blit(enemyIMG, (x, y))
 
 running = True
 while running:
@@ -28,26 +37,33 @@ while running:
         
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                playerXchange = -0.1
+                playerXchange = -0.3
             
             if event.key == pygame.K_RIGHT:
-                playerXchange = 0.1
+                playerXchange = 0.3
+            
+            # if event.key == pygame.K_UP:
+            #     playerYchange = -0.3
                 
-            if event.key == pygame.K_UP:
-                playerYchange = -0.1
-                
-            if event.key == pygame.K_DOWN:
-                playerYchange = 0.1
+            # if event.key == pygame.K_DOWN:
+            #     playerYchange = 0.3
                 
                 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerXchange = 0
             
-            if event.key == pygame.K_DOWN or event.key == pygame.K_UP:
-                playerYchange = 0
+            # if event.key == pygame.K_DOWN or event.key == pygame.K_UP:
+            #     playerYchange = 0
             
     playerX += playerXchange
-    playerY += playerYchange
+    
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736    
+    # playerY += playerYchang
+    
     player(playerX, playerY)
+    enemy(enemyX, enemyY)
     pygame.display.update()
